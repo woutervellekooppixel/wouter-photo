b88f9a7865ce37e421f02820f82dfc3dc4886cce
woutervellekooppixel <info@woutervellekoop.nl>
2025-07-05 20:45:10 +0200

    check

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -9,36 +9,42 @@ const products = [
     name: 'Zwart-wit Concertfoto',
     price: 45,
     image: '/shop/print1.jpg',
+    description: 'Een krachtige zwart-wit foto, gedrukt op mat papier.',
   },
   {
     id: 'print2',
     name: 'Backstage Moment',
     price: 55,
     image: '/shop/print2.jpg',
+    description: 'Een uniek backstage shot, vol sfeer en details.',
   },
   {
     id: 'print3',
     name: 'Dramatische Spotlights',
     price: 60,
     image: '/shop/print3.jpg',
+    description: 'Spotlights en rook zorgen voor een iconisch podiumbeeld.',
   },
   {
     id: 'print4',
     name: 'Zanger in actie',
     price: 50,
     image: '/shop/print4.jpg',
+    description: 'Energie en emotie in één moment gevangen.',
   },
   {
     id: 'print5',
     name: 'Overzichtsfoto Festival',
     price: 70,
     image: '/shop/print5.jpg',
+    description: 'Het hele festival in één indrukwekkend beeld.',
   },
   {
     id: 'print6',
     name: 'Intiem portret',
     price: 65,
     image: '/shop/print6.jpg',
+    description: 'Een teder portret met zacht licht en diepte.',
   },
 ]
 
