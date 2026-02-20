393e7f3010605e8449bc3de8320f249e6ce3541e
woutervellekooppixel <info@woutervellekoop.nl>
2026-02-20 14:41:46 +0100

    Remove GTM; keep single GA4 setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -148,17 +148,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
         
-        {/* Google Tag Manager */}
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-K55DF7SN');
-          `}
-        </Script>
-        
         {/* Google Analytics 4 (gtag) */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-SGRS9782NB"
@@ -177,15 +166,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body suppressHydrationWarning>
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           <GA4PageView />
-          <noscript>
-            <iframe
-              src="https://www.googletagmanager.com/ns.html?id=GTM-K55DF7SN"
-              height="0"
-              width="0"
-              style={{ display: 'none', visibility: 'hidden' }}
-            ></iframe>
-          </noscript>
-
           <PWAHandler />
           <Header />
           <Toaster />
