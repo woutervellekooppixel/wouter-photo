1a96692f604410023a9452accb72ea2a3f96a80b
woutervellekooppixel <info@woutervellekoop.nl>
2026-02-25 10:13:02 +0100

    Update BatchCrop Adobe Exchange URL

--- lib/adobeExchange.ts
+++ lib/adobeExchange.ts
@@ -2,7 +2,7 @@ export const ADOBE_EXCHANGE_HOME = "https://exchange.adobe.com/";
 
 export const ADOBE_EXCHANGE_BATCHCROP_URL =
 	process.env.NEXT_PUBLIC_ADOBE_EXCHANGE_BATCHCROP_URL ||
-	"https://exchange.adobe.com/apps/cc/UXP/a22459df/versions/uxp-a22459df-version-1771839979934/preview?userId=A715574D5DF09C830A495CC8%40AdobeID";
+	"https://exchange.adobe.com/apps/cc/UXP/17b43c5f/versions/uxp-17b43c5f-version-1771508221542/preview?userId=A715574D5DF09C830A495CC8%40AdobeID";
 
 export const ADOBE_EXCHANGE_EXPORT_EVERY_X_URL =
 	process.env.NEXT_PUBLIC_ADOBE_EXCHANGE_EXPORT_EVERY_X_URL ||
